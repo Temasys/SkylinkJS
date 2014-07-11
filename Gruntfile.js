315de3fde1bed06e7c40f8ae72922d83acad8f7b
lc-temasys <leticia.choo@temasys.com.sg>
2014-07-11 13:46:18 +0800

    ENH: Changes to package.json and Gruntfile.json to link to AdapterJS version 0.0.2

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function (grunt) {
 			},
 			production: {
 				src: [
-					'../AdapterJS/source/adapter.js',
+					'../node_modules/adapterjs/publish/adapter.debug.js',
 					'<%= source %>/*.js'
 				],
 				dest: '<%= production %>/skyway.debug.js'
