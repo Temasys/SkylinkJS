eece356c8f49790476d75d4d222b23dc2bffcaac
FrancoisReg <regnoult@temasys.com.sg>
2014-07-29 14:37:01 +0800

    [ENH]Multi plateform start script
    The script called by npm start was not multi plateform. This add linux
    support

--- start.sh
+++ start.sh
@@ -1,2 +1,26 @@
-open -a /Applications/Google\ Chrome.app http://localhost:8081/demo/index.html;
-
+#Must be run in background
+sleep 1
+#Detect the platform (similar to $OSTYPE)
+OS="`uname`"
+case $OS in
+  'Linux')
+    OS='Linux'
+    google-chrome http://localhost:8081/demo/index.html
+    ;;
+  'FreeBSD')
+    OS='FreeBSD'
+    alias ls='ls -G'
+    ;;
+  'WindowsNT')
+    OS='Windows'
+    ;;
+  'darwin') 
+    OS='Mac'
+    open -a /Applications/Google\ Chrome.app http://localhost:8081/demo/index.html
+    ;;
+  'SunOS')
+    OS='Solaris'
+    ;;
+  'AIX') ;;
+  *) ;;
+esac
