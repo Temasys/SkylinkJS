28ce807c70c48f3302d52cd008b1c5114b59bfcf
FrancoisReg <regnoult@temasys.com.sg>
2014-07-30 15:13:45 +0800

    [ENH]Add really simple example

---
+++ demo/app-3.js
@@ -0,0 +1,63 @@
+//------------
+// Create our Skyway object
+var SkywayDemo = new Skyway();
+
+////////////////////////////////////
+///////TO FILL
+var APPID = XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX;
+var SECRETID = "XXXXXXXXXXXXX";
+var ROOMNAME = "demo";
+var SKYWAYSERVER = "http://sgbeta.signaling.temasys.com.sg:8018/";
+///////
+///////////////////////////////////
+
+(function() {
+  var now = new Date().toISOString();
+  var hash = CryptoJS.HmacSHA1(ROOMNAME+"_500_"+now,SECRETID);//room : demo, duration: 500h
+  var credentials = encodeURIComponent(hash.toString(CryptoJS.enc.Base64));
+  SkywayDemo.init({
+    roomserver: SKYWAYSERVER,
+    appID: APPID,
+    room: ROOMNAME,
+    region: "sg",
+    credentials : {
+      startDateTime: now,
+      duration: 500,
+      credentials: credentials,
+   }
+ });
+})();
+
+//--------
+SkywayDemo.on('readyStateChange', function (state){
+	console.log("readyStateChange");
+  if(state === SkywayDemo.READY_STATE_CHANGE.COMPLETED) {
+    // SkywayDemo.getDefaultStream();
+    SkywayDemo.joinRoom({
+	  	audio: true,
+	  	video: true
+	  });
+  }
+});
+//--------
+SkywayDemo.on('mediaAccessSuccess',function (stream){
+  console.log("mediaAccessSuccess");
+  attachMediaStream(document.getElementById("myVideo"),stream);
+});
+//--------
+SkywayDemo.on('addPeerStream', function (peerID, stream){
+  console.log("addPeerStream");
+  var div = document.createElement('video');
+  div.setAttribute("style", "width: 640px; height: 480px;");
+  div.setAttribute("autoplay","autoplay");
+  div.setAttribute("id", "remote_"+peerID);
+  document.body.appendChild(div);
+  attachMediaStream(div, stream);
+});
+//--------
+SkywayDemo.on('peerLeft', function (peerID){
+  console.log("peerLeft");
+  var DOMvideo = document.getElementById("remote_"+peerID);
+  DOMvideo.src = '';
+  document.removeChild(DOMvideo);  
+});
