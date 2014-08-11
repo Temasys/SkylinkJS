920ed487071b2805fbd0535054c6992647ad7c79
FrancoisReg <regnoult@temasys.com.sg>
2014-08-11 15:59:56 +0800

    [ENH]Add Demo 3, no CSS, minimum API functions

--- demo/app-3.js
+++ demo/app-3.js
@@ -4,10 +4,10 @@ var SkywayDemo = new Skyway();
 
 ////////////////////////////////////
 ///////TO FILL
-var APPID = XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX;
-var SECRETID = "XXXXXXXXXXXXX";
+// var APPID = XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX;
+// var SECRETID = "XXXXXXXXXXXXX";
 var ROOMNAME = "demo";
-var SKYWAYSERVER = "http://sgbeta.signaling.temasys.com.sg:8018/";
+var SKYWAYSERVER = "http://api.temasys.com.sg/";
 ///////
 ///////////////////////////////////
 
@@ -47,17 +47,19 @@ SkywayDemo.on('mediaAccessSuccess',function (stream){
 //--------
 SkywayDemo.on('addPeerStream', function (peerID, stream){
   console.log("addPeerStream");
-  var div = document.createElement('video');
-  div.setAttribute("style", "width: 640px; height: 480px;");
-  div.setAttribute("autoplay","autoplay");
-  div.setAttribute("id", "remote_"+peerID);
-  document.body.appendChild(div);
-  attachMediaStream(div, stream);
+  var DOMRemoteVideo = document.createElement('video');
+  DOMRemoteVideo.setAttribute("style", "width: 320px; height: 240px;");
+  DOMRemoteVideo.setAttribute("autoplay","autoplay");
+  DOMRemoteVideo.setAttribute("id", "remote_"+peerID);
+  var DOMcontainer = document.getElementById("remoteContainer");
+  DOMcontainer.appendChild(DOMRemoteVideo);
+  attachMediaStream(DOMRemoteVideo, stream);
 });
 //--------
 SkywayDemo.on('peerLeft', function (peerID){
   console.log("peerLeft");
   var DOMvideo = document.getElementById("remote_"+peerID);
+  var DOMcontainer = document.getElementById("remoteContainer");
   DOMvideo.src = '';
-  document.body.removeChild(DOMvideo);  
+  DOMcontainer.removeChild(DOMvideo);  
 });
