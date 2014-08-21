d4706999ee5060ad4b07587cae41834154b29019
FrancoisReg <regnoult@temasys.com.sg>
2014-08-21 15:23:02 +0800

    Beautify JS

--- demo/simple-app/app.js
+++ demo/simple-app/app.js
@@ -4,57 +4,63 @@ var SkywayDemo = new Skyway();
 
 ////////////////////////////////////
 ///////TO FILL
-var APPKEYID = XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX;
+var APPKEYID = XXXXXXXX - XXXX - XXXX - XXXX - XXXXXXXXXXXX;
 var ROOMNAME = "demo";
 var SKYWAYSERVER = "http://api.temasys.com.sg/";
 ///////
 ///////////////////////////////////
 
-(function() {
-  SkywayDemo.init({
+(function()
+{
+  SkywayDemo.init(
+  {
     roomserver: SKYWAYSERVER,
     apiKey: APPKEYID,
     defaultRoom: ROOMNAME,
     region: "sg"
- });
+  });
 })();
 
 //--------
-SkywayDemo.on('readyStateChange', function (state)
+SkywayDemo.on('readyStateChange', function(state)
 {
-	console.log("readyStateChange");
-  if(state === SkywayDemo.READY_STATE_CHANGE.COMPLETED)
+  console.log("readyStateChange");
+  if (state === SkywayDemo.READY_STATE_CHANGE.COMPLETED)
   {
-    SkywayDemo.joinRoom({
-	  	audio: true,
-	  	video: true
-	  });
+    SkywayDemo.joinRoom(
+    {
+      audio: true,
+      video: true
+    });
   }
 });
 //--------
-SkywayDemo.on('mediaAccessSuccess',function (stream){
+SkywayDemo.on('mediaAccessSuccess', function(stream)
+{
   console.log("mediaAccessSuccess");
-  attachMediaStream(document.getElementById("myVideo"),stream);
+  attachMediaStream(document.getElementById("myVideo"), stream);
 });
 //--------
-SkywayDemo.on('incomingStream', function (peerID, stream, isSelf){
-	if(!isSelf)
-	{
-		console.log("addPeerStream");
-	  var DOMRemoteVideo = document.createElement('video');
-	  DOMRemoteVideo.setAttribute("style", "width: 320px; height: 240px;");
-	  DOMRemoteVideo.setAttribute("autoplay","autoplay");
-	  DOMRemoteVideo.setAttribute("id", "remote_"+peerID);
-	  var DOMcontainer = document.getElementById("remoteContainer");
-	  DOMcontainer.appendChild(DOMRemoteVideo);
-	  attachMediaStream(DOMRemoteVideo, stream);
-	}
-  
+SkywayDemo.on('incomingStream', function(peerID, stream, isSelf)
+{
+  if (!isSelf)
+  {
+    console.log("addPeerStream");
+    var DOMRemoteVideo = document.createElement('video');
+    DOMRemoteVideo.setAttribute("style", "width: 320px; height: 240px;");
+    DOMRemoteVideo.setAttribute("autoplay", "autoplay");
+    DOMRemoteVideo.setAttribute("id", "remote_" + peerID);
+    var DOMcontainer = document.getElementById("remoteContainer");
+    DOMcontainer.appendChild(DOMRemoteVideo);
+    attachMediaStream(DOMRemoteVideo, stream);
+  }
+
 });
 //--------
-SkywayDemo.on('peerLeft', function (peerID){
+SkywayDemo.on('peerLeft', function(peerID)
+{
   console.log("peerLeft");
-  var DOMvideo = document.getElementById("remote_"+peerID);
+  var DOMvideo = document.getElementById("remote_" + peerID);
   var DOMcontainer = document.getElementById("remoteContainer");
   DOMvideo.src = '';
   DOMcontainer.removeChild(DOMvideo);
