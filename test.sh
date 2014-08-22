447e572413eda05a7040228ece4ad8309f03505d
lc-temasys <leticia.choo@temasys.com.sg>
2014-08-22 14:32:03 +0800

    TEST: Updated version of test to catch invalid parameters.

--- test.sh
+++ test.sh
@@ -1,21 +1,21 @@
-echo "Running test '$1'";
+test="$1";
 grunt jshint;
 
 #Detect the type of test to run
-function get_test()
-{
-  case "$1" in
+function get_test () {
+  echo "Running test '$test'..";
+  case "$test" in
     event)
-      echo "events-test";
+      browserify tests/events-test.js | testling -x "$1";
       ;;
     start-bot)
-      echo "start-bot";
+      browserify tests/start-bot.js | testling -x "$1";
       ;;
     webrtc)
-      echo "webrtc-test";
+      browserify tests/webrtc-test.js | testling -x "$1";
       ;;
     *)
-      echo "Command not found.";
+      echo "Command '$test' not found.";
   esac
 }
 
@@ -24,20 +24,20 @@ OS="`uname`"
 case $OS in
   'Linux')
     OS='Linux'
-    browserify tests/$(get_test $1).js | testling -x "google-chrome ";
+    get_test "google-chrome ";
     ;;
   'FreeBSD')
     OS='FreeBSD'
     alias ls='ls -G'
-    browserify tests/$(get_test $1).js | testling -x "google-chrome ";
+    get_test "google-chrome ";
     ;;
   'MINGW32_NT-6.1')
     OS='Windows'
-    browserify tests/$(get_test $1).js | testling -x "'C:/Program Files/Google/Chrome/Application/chrome.exe'";
-   ;;
+    get_test "'C:/Program Files/Google/Chrome/Application/chrome.exe'";
+    ;;
   'Darwin')
     OS='Mac'
-  browserify tests/$(get_test $1).js | testling -x "open -a /Applications/Google\ Chrome.app";
+    get_test "open -a /Applications/Google\ Chrome.app";
     ;;
   'AIX') ;;
   *) echo "OS $OS not recognized";;
