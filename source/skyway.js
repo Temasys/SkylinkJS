d6328c4c70c3a0fe021c6dddf0fd816af40eaec8
tomfitango <elijah@Elijahs-Mac-Mini.local>
2014-10-07 14:50:34 -0400

    Updated to exclude IE 10
    IE 11 does not have XDomainReques, IE 10 has it but does not need.

--- source/skyway.js
+++ source/skyway.js
@@ -976,7 +976,7 @@
     this._requestServerInfo = function(method, url, callback, params) {
       var xhr = new window.XMLHttpRequest();
       /*  FOR IE 8-9 CORS REQUEST */
-      if (window.XDomainRequest)
+      if (window.XDomainRequest  && !navigator.userAgent.indexOf('IE 10'))
       {
           xhr = new XDomainRequest();
       }
