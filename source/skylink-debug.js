f21fabff6f3bf7f8630bccc1a96643910a71f82b
Leticia Choo <leticia.choo@temasys.com.sg>
2014-11-04 18:29:58 +0800

    [SJS-124]: Fix for logging appearing log object

--- source/skylink-debug.js
+++ source/skylink-debug.js
@@ -103,7 +103,6 @@ var _logFn = function(logLevel, message, debugObject) {
         console[logConsole](enableDebugOutputLog);
       }
     } else {
-      console.log(levels[logLevel], message);
       if (typeof debugObject !== 'undefined') {
         console[levels[logLevel]](outputLog, debugObject);
       } else {
