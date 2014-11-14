399ef724eb55c6ae9aa52e4ef54269780aff0428
FrancoisReg <regnoult@temasys.com.sg>
2014-11-14 17:56:50 +0800

    [ENH]Re-write chat-room example
    SJS-156

---
+++ demo/chat-room/config-example.js
@@ -0,0 +1,11 @@
+/**************************** Note **********************************
+ Save your api settings like apiKey, defaultRoom and room and save it
+ in a file called [config.js]
+*********************************************************************/
+////////////////////////////////////
+///////TO FILL
+var APPKEYID = 'XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX';
+var ROOMNAME = "demo";
+var SKYLINKSERVER = "http://api.temasys.com.sg/";
+///////
+///////////////////////////////////
