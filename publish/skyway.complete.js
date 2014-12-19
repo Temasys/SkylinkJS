5fe26755381dc6a5b83893a316e3203a22757cf4
Thanh Nguyen <tienthanh8490@gmail.com>
2014-12-19 14:20:48 +0800

    [ENH]: self._streamSettings.video=false when there is no video

--- publish/skyway.complete.js
+++ publish/skyway.complete.js
@@ -1,4 +1,4 @@
-/*! skylinkjs - v0.5.7 - 2014-12-18 */
+/*! skylinkjs - v0.5.7 - 2014-12-19 */
 
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.io=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 
@@ -7620,7 +7620,7 @@ if (navigator.mozGetUserMedia) {
     Temasys.WebRTCPlugin.pluginNeededButNotInstalledCb);
 }
 
-/*! skylinkjs - v0.5.7 - 2014-12-18 */
+/*! skylinkjs - v0.5.7 - 2014-12-19 */
 
 (function() {
 
@@ -9958,7 +9958,7 @@ Skylink.prototype._setLocalAndSendMessage = function(targetMid, sessionDescripti
     sdpLines = self._setSDPVideoResolution(sdpLines, self._streamSettings.video);
   }
   self._streamSettings.bandwidth = self._streamSettings.bandwidth || {};
-  self._streamSettings.video = self._streamSettings.video || {};
+  self._streamSettings.video = self._streamSettings.video || false;
   log.info([targetMid, null, null, 'Custom bandwidth settings:'], {
     audio: (self._streamSettings.bandwidth.audio || 'Not set') + ' kB/s',
     video: (self._streamSettings.bandwidth.video || 'Not set') + ' kB/s',
