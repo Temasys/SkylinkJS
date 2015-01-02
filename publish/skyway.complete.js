1e95c204d18cae7616121c677d4ae2a7c2efcd95
Leticia Choo <leticia.choo@temasys.com.sg>
2015-01-02 13:35:45 +0800

    [ESS-1][BUG]: Fix for sdp not defined error when enableDataChannel is false

--- publish/skyway.complete.js
+++ publish/skyway.complete.js
@@ -14714,13 +14714,13 @@ Skylink.prototype.disableVideo = function() {
 Skylink.prototype._findSDPLine = function(sdpLines, condition) {
   for (var index in sdpLines) {
     if (sdpLines.hasOwnProperty(index)) {
-      for (var c=0; c<condition.length; c++) {
-          if (sdpLines[index].indexOf(condition[c]) === 0) {
-            return [index, sdpLines[index]];
-          }
+      for (var c = 0; c < condition.length; c++) {
+        if (sdpLines[index].indexOf(condition[c]) === 0) {
+          return [index, sdpLines[index]];
         }
       }
     }
+  }
   return [];
 };
 
@@ -14802,7 +14802,7 @@ Skylink.prototype._setSDPBitrate = function(sdpLines) {
       var videoLine = this._findSDPLine(sdpLines, ['a=video', 'm=video']);
       sdpLines.splice(videoLine[0], 1, videoLine[1], 'b=AS:' + bandwidth.video);
     }
-    if (bandwidth.data) {
+    if (bandwidth.data && this._enableDataChannel) {
       var dataLine = this._findSDPLine(sdpLines, ['a=application', 'm=application']);
       sdpLines.splice(dataLine[0], 1, dataLine[1], 'b=AS:' + bandwidth.data);
     }
