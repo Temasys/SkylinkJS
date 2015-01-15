1395c8b90c76658814b5b77a91291c29d52a1f41
Thomas Gorissen <serrynaimo@me.com>
2015-01-15 13:38:56 +0800

    Bamboo trigger test2

--- Gruntfile.js
+++ Gruntfile.js
@@ -204,59 +204,58 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.registerTask('versionise',
-        'Adds version meta intormation', function() {
-            var done = this.async(),
-                arr = [];
+    grunt.registerTask('versionise', 'Adds version meta intormation', function() {
+        var done = this.async(),
+            arr = [];
 
-            grunt.util.spawn({
-                cmd: 'git',
-                args: ['log', '-1', '--pretty=format:%h\n %ci']
-            }, function(err, result) {
-                if (err) {
-                    return done(false);
-                }
-                arr = result.toString().split('\n ');
-                grunt.config('meta.rev', arr[0]);
-                grunt.config('meta.date', arr[1]);
-            });
-
-            try {
-	            var version = grunt.config('pkg.version').match('^([0-9]+)\.([0-9]+)\.([0-9]+)$')
-	            grunt.config('pkg.version_major', version[1]);
-	            grunt.config('pkg.version_minor', version[2]);
-	            grunt.config('pkg.version_release', version[3]);
-	        }
-	        catch (e) {
-	        	grunt.fatal('Version ' + grunt.config('pkg.version') + ' has not the correct format.');
-	        }
-
-            grunt.util.spawn({
-                cmd: 'git',
-                args: [
-                    'for-each-ref',
-                    '--sort=*authordate',
-                    '--format="%(tag)"',
-                    'refs/tags'
-                ]
-            }, function(err, result) {
-                if (err) {
-                    return done(false);
-                }
-                arr = result.toString().split('\n');
-
-                var tag = arr[arr.length - 1];
-                tag = tag.toString();
-                grunt.config('meta.tag', tag);
-
-                grunt.log.write('Version: ' + grunt.config('pkg.version') +
-                	'\nRevision: ' + grunt.config('meta.rev') +
-                	'\nDate: ' + grunt.config('meta.date') +
-                	'\nGit Tag: ' + grunt.config('meta.tag') + '\n');
-
-                done(result);
-            });
+        grunt.util.spawn({
+            cmd: 'git',
+            args: ['log', '-1', '--pretty=format:%h\n %ci']
+        }, function(err, result) {
+            if (err) {
+                return done(false);
+            }
+            arr = result.toString().split('\n ');
+            grunt.config('meta.rev', arr[0]);
+            grunt.config('meta.date', arr[1]);
         });
+
+        try {
+            var version = grunt.config('pkg.version').match('^([0-9]+)\.([0-9]+)\.([0-9]+)$')
+            grunt.config('pkg.version_major', version[1]);
+            grunt.config('pkg.version_minor', version[2]);
+            grunt.config('pkg.version_release', version[3]);
+        }
+        catch (e) {
+        	grunt.fatal('Version ' + grunt.config('pkg.version') + ' has not the correct format.');
+        }
+
+        grunt.util.spawn({
+            cmd: 'git',
+            args: [
+                'for-each-ref',
+                '--sort=*authordate',
+                '--format="%(tag)"',
+                'refs/tags'
+            ]
+        }, function(err, result) {
+            if (err) {
+                return done(false);
+            }
+            arr = result.toString().split('\n');
+
+            var tag = arr[arr.length - 1];
+            tag = tag.toString();
+            grunt.config('meta.tag', tag);
+
+            grunt.log.write('Version: ' + grunt.config('pkg.version') +
+            	'\nRevision: ' + grunt.config('meta.rev') +
+            	'\nDate: ' + grunt.config('meta.date') +
+            	'\nGit Tag: ' + grunt.config('meta.tag') + '\n');
+
+            done(result);
+        });
+    });
 
     grunt.registerTask('publish', [
     	'versionise',
