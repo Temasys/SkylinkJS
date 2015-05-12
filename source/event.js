08f3531900007266b9efa0de361cc6c9f8527de0
Thanh Nguyen <tienthanh8490@gmail.com>
2015-05-12 12:46:55 +0800

    ESS-223-eventModule #comment Mixin function

--- source/event.js
+++ source/event.js
@@ -16,34 +16,49 @@ var Event = {
 	}
 };
 
-Event.prototype.on = function(event, callback, context){
+Event.on = function(event, fct){
+	this._events = this._events || {};
+	this._events[event] = this._events[event]	|| [];
+	this._events[event].push(fct);
+};
+
+Event.off = function(event, callback, context){
 	return this;
 };
 
-Event.prototype.off = function(event, callback, context){
-	return this;
-};
-
-Event.prototype.offAll = function(event, callback, context){
+Event.offAll = function(event, callback, context){
 	return this;
 }
 
-Event.prototype.once = function(event, callback, context){
+Event.once = function(event, callback, context){
 	return this;
 };
 
-Event.prototype.trigger = function(event, callback, context){
-	return this;
+Event.trigger = function(event){
+	this._events = this._events || {};
+	if( event in this._events === false  )	return;
+	for(var i = 0; i < this._events[event].length; i++){
+		this._events[event][i].apply(this, Array.prototype.slice.call(arguments, 1));
+	}
 };
 
-Event.prototype.emit = function(event, callback, context){
+Event.emit = function(event, callback, context){
 	return this;
 };
 
 //Delegate all event functions to the object
+//TODO: check to exclude mixin function
 Event.mixin = function(object){
+	for (var method in Event){
+		if (Event.hasOwnProperty(method)){
+			if (typeof object === 'function'){
+				object.prototype[method]=Event[method];	
+			}
+			else{
+				object[method]=Event[method];
+			}
+		}
+	}
 	return object;
 }
-
-
 
