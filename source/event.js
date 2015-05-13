389073d2678be1a51ecb447b4ae73c020871b4ee
Thanh Nguyen <tienthanh8490@gmail.com>
2015-05-13 13:33:34 +0800

    ESS-223-eventModule #comment Add off() and refactor

--- source/event.js
+++ source/event.js
@@ -1,35 +1,62 @@
 var Event = {
+	
 	on: function(event, callback){
-		this.listeners = this.listeners || {};
-		this.listeners.on = this.listeners.on || {};
 		this.listeners.on[event] = this.listeners.on[event] || [];
     	this.listeners.on[event].push(callback);
 		return this;
 	},
+
 	off: function(event, callback){
+
+		if (typeof callback === 'undefined'){
+
+			this.listeners.on[event]=[];
+			this.listeners.once[event]=[];
+		}
+		else{
+			if (this.listeners.on[event]){
+				for (var i=0; i<this.listeners.on[event].length; i++){
+					if (this.listeners.on[event][i] === callback){
+						this.listeners.on[event].splice(i,1);
+						break;
+					}
+				}
+			}
+		
+			if (this.listeners.once[event]){
+				for (var i=0; i<this.listeners.once[event].length; i++){
+					if (this.listeners.once[event][i] === callback){
+						this.listeners.once[event].splice(i,1);
+						break;
+					}
+				}
+			}
+		}
 		return this;
 	},
+
 	once: function(event, callback){
-		this.listeners = this.listeners || {};
-		this.listeners.once = this.listeners.once || {};
 		this.listeners.once[event] = this.listeners.once[event] || [];
     	this.listeners.once[event].push(callback);
 		return this;
 	},
+
 	trigger: function(event){
 		var args = Array.prototype.slice.call(arguments,1);
-		var onListeners = this.listeners ? (this.listeners.on ? this.listeners.on[event] : []) : [];
-		var onceListeners = this.listeners ? (this.listeners.once ? this.listeners.once[event] : []) : [];
 
-	    for (var i=0; i<onListeners.length; i++) {
-	    	onListeners[i].apply(this, args);
-	    }
+		if (this.listeners.on[event]){
+			for (var i=0; i<this.listeners.on[event].length; i++) {
+		    	this.listeners.on[event][i].apply(this, args);
+		    }
+		}
 
-	    for (var i=0; i<onceListeners.length; i++){
-	    	onceListeners[i].apply(this, args);
-	    	onceListeners.splice(i,1);
-	    	i--;
-	    }
+		if (this.listeners.once[event]){
+			for (var i=0; i<this.listeners.once[event].length; i++){
+		    	this.listeners.once[event][i].apply(this, args);
+		    	this.listeners.once[event].splice(i,1);
+		    	i--;
+		    }
+		}
 
 		return this;
 	},
@@ -45,6 +72,12 @@ var Event = {
 				}
 			}
 		}
+
+		object.listeners = {
+			on: {},
+			once: {}
+		}
+
 		return object;
 	}
 };
