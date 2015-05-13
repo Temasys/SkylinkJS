e145ee6459987df99ab8ee9d243e0d4f3b750e65
Thanh Nguyen <tienthanh8490@gmail.com>
2015-05-13 12:38:00 +0800

    ESS-223-eventModule #comment Add once()

--- source/event.js
+++ source/event.js
@@ -10,15 +10,27 @@ var Event = {
 		return this;
 	},
 	once: function(event, callback){
+		this.listeners = this.listeners || {};
+		this.listeners.once = this.listeners.once || {};
+		this.listeners.once[event] = this.listeners.once[event] || [];
+    	this.listeners.once[event].push(callback);
 		return this;
 	},
 	trigger: function(event){
-
 		var args = Array.prototype.slice.call(arguments,1);
 		var onListeners = this.listeners ? (this.listeners.on ? this.listeners.on[event] : []) : [];
-	    for (var i = 0; i < onListeners.length; i++) {
+		var onceListeners = this.listeners ? (this.listeners.once ? this.listeners.once[event] : []) : [];
+
+	    for (var i=0; i<onListeners.length; i++) {
 	    	onListeners[i].apply(this, args);
 	    }
+
+	    for (var i=0; i<onceListeners.length; i++){
+	    	onceListeners[i].apply(this, args);
+	    	onceListeners.splice(i,1);
+	    	i--;
+	    }
+
 		return this;
 	},
 	mixin: function(object){
