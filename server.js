2670fa7d55bf448c0e51bf8b54e3b2ed8ae83985
Sherwin Sim <sherwin.sim@temasys.com.sg>
2015-06-04 19:20:19 -0400

    [ENH]Don't error out if you don't have and certificates for HTTPS

--- server.js
+++ server.js
@@ -7,8 +7,18 @@ var app = connect();
 app.use(connect.static(__dirname));
 
 http.createServer(app).listen(8081);
-https.createServer({
-  key: fs.readFileSync('certificates/server.key'),
-  cert: fs.readFileSync('certificates/server.crt')
-}, app).listen(8082);
-console.log("Server start @ 8081 (HTTP) 8082 (HTTPS)");
+console.log("Server start @ 8081 (HTTP)");
+
+fs.stat('certificates/server.key', function(err, stat) {
+    if(err == null) {
+      https.createServer({
+        key: fs.readFileSync('certificates/server.key'),
+        cert: fs.readFileSync('certificates/server.crt')
+      }, app).listen(8082);
+      console.log("Server start @ 8082 (HTTPS)");
+    } else {
+        console.warn('Server cannot start @ 8082 (HTTPS)'
+        + ' Certificate File error: ' + err.code 
+        + ' for certificates/server.key'); 
+    }
+});
