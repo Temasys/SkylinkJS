2d314a622e0f038cf1a40be05ec9166618d0e9e8
Leticia Choo <leticia.choo@temasys.com.sg>
2015-07-06 11:07:51 +0800

    ESS-296 #comment Added constructor tests to Stream test scripts.

---
+++ tests/spec/stream/constructor.js
@@ -0,0 +1,25 @@
+describe('new Stream()', function () {
+
+  var stream = null;
+
+  it('does not throw an error', function (done) {
+    this.timeout(testItemTimeout);
+
+    expect(function () {
+      stream = new Stream();
+    }).to.not.throw(Error);
+
+    done();
+  });
+
+  it('returns a new Stream object', function (done) {
+    this.timeout(testItemTimeout);
+
+    (typeof stream).should.be.eql('object');
+
+    assert.instanceOf(stream, Stream);
+
+    done();
+  });
+
+});
