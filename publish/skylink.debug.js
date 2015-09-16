d790726c5ede88dfa59de19c079a72cad555898b
Leticia Choo <leticia.choo@temasys.com.sg>
2015-09-16 17:38:00 +0800

    ESS-360 #comment Polyfills for IE 8-9 workarounds for Skylink.

--- publish/skylink.debug.js
+++ publish/skylink.debug.js
@@ -1,8 +1,97 @@
-/*! skylinkjs - v0.6.1 - Wed Sep 16 2015 16:35:58 GMT+0800 (SGT) */
+/*! skylinkjs - v0.6.1 - Wed Sep 16 2015 17:37:49 GMT+0800 (SGT) */
 
 (function() {
 
 'use strict';
+
+/* IE < 10 Polyfills */
+// Mozilla provided polyfill for Object.keys()
+if (!Object.keys) {
+  Object.keys = (function() {
+    var hasOwnProperty = Object.prototype.hasOwnProperty,
+      hasDontEnumBug = !({
+        toString: null
+      }).propertyIsEnumerable('toString'),
+      dontEnums = [
+        'toString',
+        'toLocaleString',
+        'valueOf',
+        'hasOwnProperty',
+        'isPrototypeOf',
+        'propertyIsEnumerable',
+        'constructor'
+      ],
+      dontEnumsLength = dontEnums.length;
+
+    return function(obj) {
+      if (typeof obj !== 'object' && typeof obj !== 'function' || obj === null) throw new TypeError('Object.keys called on non-object');
+
+      var result = [];
+
+      for (var prop in obj) {
+        if (hasOwnProperty.call(obj, prop)) result.push(prop);
+      }
+
+      if (hasDontEnumBug) {
+        for (var i = 0; i < dontEnumsLength; i++) {
+          if (hasOwnProperty.call(obj, dontEnums[i])) result.push(dontEnums[i]);
+        }
+      }
+      return result;
+    }
+  })()
+};
+
+// Mozilla provided polyfill for Date.getISOString()
+(function() {
+  function pad(number) {
+    if (number < 10) {
+      return '0' + number;
+    }
+    return number;
+  }
+
+  Date.prototype.toISOString = function() {
+    return this.getUTCFullYear() +
+      '-' + pad(this.getUTCMonth() + 1) +
+      '-' + pad(this.getUTCDate()) +
+      'T' + pad(this.getUTCHours()) +
+      ':' + pad(this.getUTCMinutes()) +
+      ':' + pad(this.getUTCSeconds()) +
+      '.' + (this.getUTCMilliseconds() / 1000).toFixed(3).slice(2, 5) +
+      'Z';
+  };
+})();
+
+// addEventListener polyfill 1.0 / Eirik Backer / MIT Licence
+(function(win, doc){
+  if(win.addEventListener) return; //No need to polyfill
+
+  function docHijack(p){var old = doc[p];doc[p] = function(v){ return addListen(old(v)) }}
+  function addEvent(on, fn, self){
+    return (self = this).attachEvent('on' + on, function(e){
+      var e = e || win.event;
+      e.preventDefault  = e.preventDefault  || function(){e.returnValue = false}
+      e.stopPropagation = e.stopPropagation || function(){e.cancelBubble = true}
+      fn.call(self, e);
+    });
+  }
+  function addListen(obj, i){
+    if(i = obj.length)while(i--)obj[i].addEventListener = addEvent;
+    else obj.addEventListener = addEvent;
+    return obj;
+  }
+
+  addListen([doc, win]);
+  if('Element' in win)win.Element.prototype.addEventListener = addEvent; //IE8
+  else{                                     //IE < 8
+    doc.attachEvent('onreadystatechange', function(){addListen(doc.all)}); //Make sure we also init at domReady
+    docHijack('getElementsByTagName');
+    docHijack('getElementById');
+    docHijack('createElement');
+    addListen(doc.all);
+  }
+})(window, document);
 
 /**
  * Please refer to the {{#crossLink "Skylink/init:method"}}init(){{/crossLink}}
@@ -71,11 +160,13 @@ function Skylink() {
    * @for Skylink
    * @since 0.5.9
    */
-  this.generateUUID  = function () {
+  this.generateUUID = function() {
     var d = new Date().getTime();
     var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = (d + Math.random()*16)%16 | 0; d = Math.floor(d/16); return (c=='x' ? r : (r&0x7|0x8)).toString(16); }
-    );
+      var r = (d + Math.random() * 16) % 16 | 0;
+      d = Math.floor(d / 16);
+      return (c == 'x' ? r : (r & 0x7 | 0x8)).toString(16);
+    });
     return uuid;
   };
 }
