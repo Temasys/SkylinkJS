89f82f893a4bb6e3849339e94d61c867c7d75692
Thanh Nguyen <tienthanh8490@gmail.com>
2015-09-23 18:41:18 +0800

    ESS-370-introducePrivileged #comment Update peer-privileged

--- source/peer-privileged.js
+++ source/peer-privileged.js
@@ -2,9 +2,9 @@
  * The types of Privileged states available
  * @attribute PRIVILEGED_STATE
  * @type JSON
- * @param {String} ENQUIRED Privileged peer already enquired signaling for list of unprivileged peers
- * @param {String} RECEIVED Privileged peer received list of unprivileged peers from signaling
- * @param {String} INTRODUCED Privileged peer introduced 2 unprivileged peers to each other
+ * @param {String} ENQUIRED Privileged peer already enquired signaling for list of peers
+ * @param {String} RECEIVED Privileged peer received list of peers from signaling
+ * @param {String} ERROR Error happened during peer introduction
  * @readOnly
  * @component Peer
  * @for Skylink
@@ -13,7 +13,6 @@
 Skylink.prototype.PRIVILEGED_STATE = {
 	ENQUIRED: 'enquired',
 	RECEIVED: 'received',
-	INTRODUCED: 'introduced',
 	ERROR: 'error'
 };
 
@@ -55,8 +54,8 @@ Skylink.prototype._isPrivileged = false;
 Skylink.prototype._parentKey = null;
 
 /**
- * List of unprivileged peers retrieved from signaling
- * @attribute _unprivilegedPeerList
+ * List of peers retrieved from signaling
+ * @attribute _peerList
  * @type Object
  * @default null
  * @private
@@ -64,18 +63,19 @@ Skylink.prototype._parentKey = null;
  * @for Skylink
  * @since 0.6.1
  */
-Skylink.prototype._unprivilegedPeerList = null;
+Skylink.prototype._peerList = null;
 
 /**
- * For privileged user to enquire signaling to return the list of unprivileged peers under the same parent.
- * @method getUnprivilegedPeers
- * @param {Function} [callback] Callback when unprivileged peer list was returned
+ * For privileged user to enquire signaling to return the list of peers under the same parent.
+ * @method getPeers
+ * @param {Boolean} [showAll=false] If true, include privileged peers in the list. False by default.
+ * @param {Function} [callback] Callback when peer list was returned
  * @public
  * @component Peer
  * @for Skylink
  * @since 0.6.1
  */
-Skylink.prototype.getUnprivilegedPeers = function(callback){
+Skylink.prototype.getPeers = function(showAll, callback){
 	var self = this;
 	if (!self._isPrivileged){
 		log.warn('Please upgrade your key to privileged to use this function');
@@ -89,18 +89,26 @@ Skylink.prototype.getUnprivilegedPeers = function(callback){
 		log.warn('Parent key is not defined. Please authenticate again.');
 		return;	
 	}
+
+	// Only callback is provided
+	if (typeof showAll === 'function'){
+		callback = showAll;
+		showAll = false;
+	}
+
 	self._sendChannelMessage({
-		type: self._SIG_MESSAGE_TYPE.GET_UNPRIVILEGED,
+		type: self._SIG_MESSAGE_TYPE.GET_PEERS,
 		privilegedKey: self._appKey,
-		parentKey: self._parentKey
+		parentKey: self._parentKey,
+		showAll: showAll || false
 	});
 	self._trigger('privilegedStateChange',self.PRIVILEGED_STATE.ENQUIRED, self._user.sid, null, null, null);
-	log.log('Enquired signaling for unprivileged peers');
+	log.log('Enquired signaling for peers within the realm');
 
 	if (typeof callback === 'function'){
-		self.once('privilegedStateChange', function(state, privilegedPeerId, sendingPeerId, receivingPeerId, unprivilegedPeerList){
-			callback(null, unprivilegedPeerList);
-		}, function(state, privilegedPeerId, sendingPeerId, receivingPeerId, unprivilegedPeerList){
+		self.once('privilegedStateChange', function(state, privilegedPeerId, sendingPeerId, receivingPeerId, peerList){
+			callback(null, peerList);
+		}, function(state, privilegedPeerId, sendingPeerId, receivingPeerId, peerList){
 			return state === self.PRIVILEGED_STATE.RECEIVED;
 		});
 	}
@@ -108,7 +116,7 @@ Skylink.prototype.getUnprivilegedPeers = function(callback){
 };
 
 /**
- * For privileged peer to introduce 2 unprivileged peers to each other
+ * For privileged peer to introduce 2 peers to each other
  * @method introducePeer
  * @param {String} sendingPeerId Id of the peer who sends enter
  * @param {String} receivingPeerId Id of the peer who receives enter
@@ -128,7 +136,7 @@ Skylink.prototype.introducePeer = function(sendingPeerId, receivingPeerId){
 		sendingPeerId: sendingPeerId,
 		receivingPeerId: receivingPeerId
 	});
-	self._trigger('privilegedStateChange', self.PRIVILEGED_STATE.INTRODUCE, self._user.sid, sendingPeerId, receivingPeerId, self._unprivilegedPeerList);
+	self._trigger('privilegedStateChange', self.PRIVILEGED_STATE.INTRODUCE, self._user.sid, sendingPeerId, receivingPeerId, self._peerList);
 	log.log('Introducing',sendingPeerId,'to',receivingPeerId);
 };
 
