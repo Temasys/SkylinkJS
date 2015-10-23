d5f571e31fb8195cda28a6faa15444ae26eea8dc
Thanh Nguyen <tienthanh8490@gmail.com>
2015-10-23 16:10:54 +0800

    ESS-385-stable-fixes #comment Use https for start script

--- start.sh
+++ start.sh
@@ -5,20 +5,20 @@ OS="`uname`"
 case $OS in
   'Linux')
     OS='Linux'
-    google-chrome http://localhost:8081/demo/index.html
+    google-chrome https://localhost:8082/demo/index.html
     ;;
   'FreeBSD')
     OS='FreeBSD'
     alias ls='ls -G'
-	google-chrome http://localhost:8081/demo/index.html
+	google-chrome https://localhost:8082/demo/index.html
     ;;
   'MINGW32_NT-6.1')
     OS='Windows'
-	start chrome "http://localhost:8081/demo/index.html"
+	start chrome "https://localhost:8082/demo/index.html"
     ;;
   'Darwin')
     OS='Mac'
-    open -a /Applications/Google\ Chrome.app http://localhost:8081/demo/index.html
+    open -a /Applications/Google\ Chrome.app https://localhost:8082/demo/index.html
     ;;
   'AIX') ;;
   *) echo "OS $OS not recognized";;
