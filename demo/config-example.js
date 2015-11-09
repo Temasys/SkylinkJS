553ad73e803a2a56680081dae5c23456645a10e5
johache <jo.hache@temasys.com.sg>
2015-11-09 12:49:12 +0800

    Typos in the ReadMe and in config-example.js

--- demo/config-example.js
+++ demo/config-example.js
@@ -4,7 +4,7 @@
 *********************************************************************/
 var SkylinkDemo = new Skylink();
 SkylinkDemo.setLogLevel(SkylinkDemo.LOG_LEVEL.WARN);
-var Config = {
+var config = {
   apiKey:"XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX",
   defaultRoom:"MY_DEFAULT_ROOM"
 };
