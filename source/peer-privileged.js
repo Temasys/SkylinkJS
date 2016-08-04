d0d2f2897f9c40529e48b3f8eaacfc5bf003b261
Leticia Choo <leticiachoo@Leticias-MacBook-Pro.local>
2016-08-04 21:00:02 +0800

    ESS-588 Cleanup of "peer-privileged.js" file docs.

--- source/peer-privileged.js
+++ source/peer-privileged.js
@@ -3,13 +3,15 @@
  *   Note that this feature requires <code>"isPrivileged"</code> flag to be enabled for the App Key, as
  *   only Users connecting using the App Key with this flag enabled (which we call Privileged Users / Peers)
  *   can retrieve the list of Peer IDs from Rooms within the same App space.
+ *   <a href="http://support.temasys.com.sg/support/solutions/articles/12000012342-what-is-a-privileged-key-">
+ *   Read more about Privileged App Key feature here</a>.
  * </blockquote>
  * Contains the list of <a href="#method_getPeers"><code>getPeers()</code> method</a> retrieval states.
  * @attribute GET_PEERS_STATE
  * @param {String} ENQUIRED <small>Value <code>"enquired"</code></small>
  *   The state when retrieving the list of Peer IDs from Signaling.
  * @param {String} RECEIVED <small>Value <code>"received"</code></small>
- *   The state when successfully retrieved the list of Peer IDs from Signaling.
+ *   The state when the list of Peer IDs has been successfully retrieved from Signaling.
  * @type JSON
  * @readOnly
  * @for Skylink
@@ -26,14 +28,15 @@ Skylink.prototype.GET_PEERS_STATE = {
  *   <code>"autoIntroduce"</code> flag to be disabled for the App Key, as only Users connecting
  *   using the App Key with this flag enabled (which we call Privileged Users / Peers)
  *   can introduce Peers in the same App space from other Rooms.
+ *   <a href="http://support.temasys.com.sg/support/solutions/articles/12000012342-what-is-a-privileged-key-">
+ *   Read more about Privileged App Key feature here</a>.
  * </blockquote>
  * Contains the list of <a href="#method_introducePeer"><code>introducePeer</code> method</a> introduction states.
  * @attribute INTRODUCE_STATE
  * @param {String} INTRODUCING <small>Value <code>"enquired"</code></small>
- *   The state when the privileged Peer have sent the introduction signal.
+ *   The state when request to introduce the pair of Peers has been made to the Signaling.
  * @param {String} ERROR <small>Value <code>"error"</code></small>
- *   The state when the Peer introduction has occurred an exception.
- * @type JSON
+ *   The state when request to introduce the pair of Peers has failed.
  * @readOnly
  * @for Skylink
  * @since 0.6.1
@@ -44,88 +47,63 @@ Skylink.prototype.INTRODUCE_STATE = {
 };
 
 /**
- * Whether this user automatically introduce to other peers.
+ * Stores the flag that indicates if "autoIntroduce" is enabled.
+ * If enabled, the Peers connecting the same Room will receive each others "enter" message ping.
  * @attribute _autoIntroduce
  * @type Boolean
  * @default true
  * @private
- * @component Peer
  * @for Skylink
  * @since 0.6.1
  */
 Skylink.prototype._autoIntroduce = true;
 
 /**
- * Whether this user is a privileged user.
+ * Stores the flag that indicates if "isPrivileged" is enabled.
+ * If enabled, the User has Privileged features which has the ability to retrieve the list of
+ *   Peers in the same App space with <code>getPeers()</code> method 
+ *   and introduce Peers to each other with <code>introducePeer</code> method.
  * @attribute isPrivileged
  * @type Boolean
  * @default false
  * @private
- * @component Peer
  * @for Skylink
  * @since 0.6.1
  */
 Skylink.prototype._isPrivileged = false;
 
 /**
- * List of peers retrieved from signaling
+ * Stores the list of Peers retrieved from the Signaling from <code>getPeers()</code> method.
  * @attribute _peerList
- * @type Object
- * @default null
+ * @type JSON
  * @private
- * @component Peer
  * @for Skylink
  * @since 0.6.1
  */
 Skylink.prototype._peerList = null;
 
 /**
- * Retrieves the list of rooms and peers under the same realm based
- *   on the Application Key configured in {{#crossLink "Skylink/init:method"}}init(){{/crossLink}}
- *   from the platform signaling.
- * This will only work if self is a privileged Peer.
+ * <blockquote class="info">
+ *   Note that this feature requires <code>"isPrivileged"</code> flag to be enabled for the App Key, as
+ *   only Users connecting using the App Key with this flag enabled (which we call Privileged Users / Peers)
+ *   can retrieve the list of Peer IDs from Rooms within the same App space.
+ *   <a href="http://support.temasys.com.sg/support/solutions/articles/12000012342-what-is-a-privileged-key-">
+ *   Read more about Privileged App Key feature here</a>.
+ * </blockquote>
+ * Function that retrieves the list of Peer IDs from Rooms within in the same App space.
  * @method getPeers
- * @param {Boolean} [showAll=false] The flag that indicates if returned list should
- *   also include privileged and standard in the list. By default, the value is <code>false</code>.
- *   Which means only unprivileged peers' ID (isPrivileged = autoIntroduce = false) is included.
- * @param {Function} [callback] The callback fired after the receiving the current
- *   list of Peers from platform signaling or have met with an exception.
- *   The callback signature is <code>function (error, success)</code>.
- * @param {Object} callback.error The error object received in the callback.
- *   This is the exception thrown that caused the failure for getting self user media.
- *   If received as <code>null</code>, it means that there is no errors.
- * @param {JSON} callback.success The success object received in the callback.
- *   If received as <code>null</code>, it means that there are errors.
- * @example
- *
- *   // To get list of unprivileged peers only
- *   SkylinkDemo.getPeers();
- *
- *   // To get list of all peers, including other privileged peers
- *   SkylinkDemo.getPeers(true);
- *
- *   // To get a list of unprivileged peers then invoke the callback
- *   SkylinkDemo.getPeers(function(error, success){
- *     if (error){
- *       console.log("Error happened. Can not retrieve list of peers");
- *     }
- *     else{
- *       console.log("Success fully retrieved list of peers", success);
- *     }
- *   });
- *
- *   // To get a list of all peers then invoke the callback
- *   SkylinkDemo.getPeers(true, function(error, success){
- *     if (error){
- *       console.log("Error happened. Can not retrieve list of peers");
- *     }
- *     else{
- *       console.log("Success fully retrieved list of peers", success);
- *     }
- *   });
- *
+ * @param {Boolean} [showAll=false] The flag that indicates if Signaling should also retrieve
+ *   the list of the Privileged Peer IDs.
+ * <small>By default, the Signaling doesn't return the Privileged Peer IDs</small>
+ * @param {Function} [callback] The callback function fired when request has completed.
+ *   <small>Function parameters signature is <code>function (error, success)</code></small>
+ * @param {Error|String} callback.error The error result in request.
+ *   <small>Defined as <code>null</code> when there are no errors in request</small>
+ * @param {JSON} callback.success The success result in request.
+ *   <small>Defined as <code>null</code> when there are errors in request</small>
+ *   <small>Object signature matches the <code>peerList</code> parameter payload received in the
+ *      <a href="#event_getPeersStateChange"><code>getPeersStateChange</code> event</a>.</small>
  * @trigger getPeersStateChange
- * @component Peer
  * @for Skylink
  * @since 0.6.1
  */
@@ -166,15 +144,19 @@ Skylink.prototype.getPeers = function(showAll, callback){
 };
 
 /**
- * Introduces two Peers to each other to start a connection with each other.
- * This will only work if self is a privileged Peer.
+ * <blockquote class="info">
+ *   Note that this feature requires <code>"isPrivileged"</code> flag to be enabled and
+ *   <code>"autoIntroduce"</code> flag to be disabled for the App Key, as only Users connecting
+ *   using the App Key with this flag enabled (which we call Privileged Users / Peers)
+ *   can introduce Peers in the same App space from other Rooms.
+ *   <a href="http://support.temasys.com.sg/support/solutions/articles/12000012342-what-is-a-privileged-key-">
+ *   Read more about Privileged App Key feature here</a>.
+ * </blockquote>
+ * Function that introduces a pair of Peers to start Peer connection with each other.
  * @method introducePeer
- * @param {String} sendingPeerId The Peer ID of the peer
- *   that initiates the connection with the introduced Peer.
- * @param {String} receivingPeerId The Peer ID of the
- *   introduced peer who would be introduced to the initiator Peer.
+ * @param {String} sendingPeerId The Peer ID of the Peer that will start Peer connection with <code>receivingPeerId</code>.
+ * @param {String} receivingPeerId The Peer ID of the Peer that will be connected with <code>sendingPeerId</code>.
  * @trigger introduceStateChange
- * @component Peer
  * @for Skylink
  * @since 0.6.1
  */
