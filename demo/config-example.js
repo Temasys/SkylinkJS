8c1210edfcb4a3aafa41fe8a7cac311ce9d7628a
Leticia Choo <leticia.choo@temasys.com.sg>
2016-09-06 23:53:49 +0800

    ESS-588 Update of config.js file.

--- demo/config-example.js
+++ demo/config-example.js
@@ -11,19 +11,41 @@ var config = {
   forceSSL: true
 };
 
+/**
+ * For using credentials based authentication
+ */
+var secret = null; // 'xxxxx' Use App Key secret
+var duration = 2; // 2 hours. Default is 24 for CORS auth
+var startDateTimeStamp = (new Date ()).toISOString();
+
 // Setup App Key for Privileged User Feature (for Privileged App Key + Auto Introduce Enabled)
 if (window.location.pathname === '/demo/privileged/auto-priv/') {
   config.appKey = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX';
+  secret = null; // 'xxxxx' Use App Key secret
 
 // Setup App Key for Privileged User Feature (for non-Privileged App Key + Auto Introduce Enabled)
 } else if (window.location.pathname === '/demo/privileged/auto-unpriv/') {
   config.appKey = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX';
+  secret = null; // 'xxxxx' Use App Key secret
 
 // Setup App Key for Privileged User Feature (for Privileged App Key + Auto Introduce Disabled)
 } else if (window.location.pathname === '/demo/privileged/unauto-priv/') {
   config.appKey = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX';
+  secret = null; // 'xxxxx' Use App Key secret
 
 // Setup App Key for Privileged User Feature (for non-Privileged App Key + Auto Introduce Disabled)
 } else if (window.location.pathname === '/demo/privileged/unauto-unpriv/') {
   config.appKey = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX';
+  secret = null; // 'xxxxx' Use App Key secret
+}
+
+if (secret) {
+  var genHashForCredentials = CryptoJS.HmacSHA1(config.defaultRoom + '_' + duration + '_' + startDateTimeStamp, secret);
+  var credentials = encodeURIComponent(genHashForCredentials.toString(CryptoJS.enc.Base64));
+
+  config.credentials = {
+    duration: duration,
+    startDateTime: startDateTimeStamp,
+    credentials: credentials
+  };
 }
