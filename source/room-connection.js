08849cdd21a0185986463f44b8e50d8217472399
Leticia Choo <leticia.choo@temasys.com.sg>
2016-09-21 00:47:06 +0800

    ESS-426 Updates to "room-connection" docs.

--- source/room-connection.js
+++ source/room-connection.js
@@ -231,26 +231,69 @@ Skylink.prototype._inRoom = false;
  *     console.log("User connected with correct user data?", success.peerInfo.userData.username === data.username);
  *   });
  * @trigger <ol class="desc-seq">
- *   <li>When requested Room name provided is different from currently retrieved Room session token Room name <ol>
- *   <li>Invokes <a href="#method_init"><code>init()</code> method</a> to retrieve Room session token.</li></ol></li>
- *   <li>Starts a socket connection with the Signaling server.<ol>
- *   <li>When socket connection to Signaling server is successfully established <ol>
- *   <li><a href="#event_channelOpen"><code>channelOpen</code> event</a> triggers.</li>
- *   <li><a href="#event_channelMessage"><code>channelMessage</code> event</a> triggers.</li>
- *   <li>When Peer has succesfully joined the Room <ol>
+ *   <li>If User is in a Room: <ol>
+ *   <li>Invoke <a href="#method_leaveRoom"><code>leaveRoom()</code> method</a>
+ *   to end current Room connection. <small>Invoked <a href="#method_leaveRoom"><code>leaveRoom()</code>
+ *   method</a> <code>stopMediaOptions</code> parameter value will be <code>false</code>.</small>
+ *   <small>Regardless of request errors, <code>joinRoom()</code> will still proceed.</small></li></ol></li>
+ *   <li>Check if Room name provided matches the Room name of the currently retrieved Room session token. <ol>
+ *   <li>If Room name does not matches: <ol>
+ *   <li>Invoke <a href="#method_init"><code>init()</code> method</a> to retrieve new Room session token. <ol>
+ *   <li>If request has errors: <ol><li><b>ABORT</b> and return error.</li></ol></li></ol></li></ol></li></ol></li>
+ *   <li>Open a new socket connection to Signaling server. <ol><li>If Socket connection fails: <ol>
+ *   <li><a href="#event_socketError"><code>socketError</code> event</a> triggers parameter payload
+ *   <code>errorCode</code> as <code>CONNECTION_FAILED</code>. <ol>
+ *   <li>Checks if there are fallback ports and transports to use. <ol>
+ *   <li>If there are still fallback ports and transports: <ol>
+ *   <li>Attempts to retry socket connection to Signaling server. <ol>
+ *   <li><a href="#event_channelRetry"><code>channelRetry</code> event</a> triggers.</li>
+ *   <li><a href="#event_socketError"><code>socketError</code> event</a> triggers parameter
+ *   payload <code>errorCode</code> as <code>RECONNECTION_ATTEMPT</code>.</li>
+ *   <li>If attempt to retry socket connection to Signaling server has failed: <ol>
+ *   <li><a href="#event_socketError"><code>socketError</code> event</a> triggers parameter payload
+ *   <code>errorCode</code> as <code>RECONNECTION_FAILED</code>.</li>
+ *   <li>Checks if there are still any more fallback ports and transports to use. <ol>
+ *   <li>If there are is no more fallback ports and transports to use: <ol>
+ *   <li><a href="#event_socketError"><code>socketError</code> event</a> triggers
+ *   parameter payload <code>errorCode</code> as <code>RECONNECTION_ABORTED</code>.</li>
+ *   <li><b>ABORT</b> and return error.</li></ol></li><li>Else: <ol><li><b>REPEAT</b> attempt to retry socket connection
+ *   to Signaling server step.</li></ol></li></ol></li></ol></li></ol></li></ol></li><li>Else: <ol>
+ *   <li><a href="#event_socketError"><code>socketError</code> event</a> triggers
+ *   parameter payload <code>errorCode</code> as <code>CONNECTION_ABORTED</code>.</li>
+ *   <li><b>ABORT</b> and return error.</li></ol></li></ol></li></ol></li></ol></li>
+ *   <li>If socket connection to Signaling server has opened: <ol>
+ *   <li><a href="#event_channelOpen"><code>channelOpen</code> event</a> triggers.</li></ol></li></ol></li>
+ *   <li>Checks if there is <code>options.manualGetUserMedia</code> requested <ol><li>If it is requested: <ol>
+ *   <li><a href="#event_mediaAccessRequired"><code>mediaAccessRequired</code> event</a> triggers.</li>
+ *   <li>If more than 30 seconds has passed and no <a href="#method_getUserMedia"><code>getUserMedia()</code> Stream</a>
+ *   or <a href="#method_shareScreen"><code>shareScreen()</code> Stream</a>
+ *   has been retrieved: <ol><li><b>ABORT</b> and return error.</li></ol></li></ol></li><li>Else: <ol>
+ *   <li>If there is <code>options.audio</code> or <code>options.video</code> requested: <ol>
+ *   <li>Invoke <a href="#method_getUserMedia"><code>getUserMedia()</code> method</a>. <ol>
+ *   <li>If request has errors: <ol><li><b>ABORT</b> and return error.</li></ol></li></ol></li></ol></li></ol></li>
+ *   </ol></li><li>Starts the Room session <ol><li>If Room session has started successfully: <ol>
  *   <li><a href="#event_peerJoined"><code>peerJoined</code> event</a> triggers parameter payload
  *   <code>isSelf</code> value as <code>true</code>.</li>
- *   <li>If MCU is enabled for the App Key <ol>
- *   <li><a href="#event_serverPeerJoined"><code>serverPeerJoined</code> event</a> triggers.
- *   <small>MCU has to be present in the Room in order for Peer connections to commence.</small></li></ol></li>
- *   <li>If there is <a href="#method_shareScreen"><code>shareScreen()</code> Stream</a> or
- *   <a href="#method_getUserMedia"><code>getUserMedia()</code> Stream</a> available <ol>
- *   <li><a href="#event_incomingStream"><code>incomingStream</code> event</a> triggers.
- *   <small>If
- *   <a href="#method_shareScreen"><code>shareScreen()</code> Stream</a> is available despite having
- *   <a href="#method_getUserMedia"><code>getUserMedia()</code> Stream</a> available, the
- *   <a href="#method_shareScreen"><code>shareScreen()</code> Stream</a> is sent instead of the
- *   <a href="#method_getUserMedia"><code>getUserMedia()</code> Stream</a> to Peers.</small></li></ol></li></ol>
+ *   <li>If MCU is enabled for the App Key provided in <a href="#method_init"><code>init()</code>
+ *   method</a> and connected: <ol><li><a href="#event_serverPeerJoined"><code>serverPeerJoined</code>
+ *   event</a> triggers <code>serverPeerType</code> as <code>MCU</code>. <small>MCU has
+ *   to be present in the Room in order for Peer connections to commence.</small></li>
+ *   <li>Checks for any available Stream <ol>
+ *   <li>If <a href="#method_shareScreen"><code>shareScreen()</code> Stream</a> is available: <ol>
+ *   <li><a href="#event_incomingStream"><code>incomingStream</code> event</a>
+ *   triggers parameter payload <code>isSelf</code> value as <code>true</code> and <code>stream</code>
+ *   as <a href="#method_shareScreen"><code>shareScreen()</code> Stream</a>.
+ *   <small>User will be sending <a href="#method_shareScreen"><code>shareScreen()</code> Stream</a>
+ *   to Peers.</small></li></ol></li>
+ *   <li>Else if <a href="#method_getUserMedia"><code>getUserMedia()</code> Stream</a> is available: <ol>
+ *   <li><a href="#event_incomingStream"><code>incomingStream</code> event</a> triggers parameter
+ *   payload <code>isSelf</code> value as <code>true</code> and <code>stream</code> as
+ *   <a href="#method_getUserMedia"><code>getUserMedia()</code> Stream</a>.
+ *   <small>User will be sending <code>getUserMedia()</code> Stream to Peers.</small></li></ol></li><li>Else: <ol>
+ *   <li>No Stream will be sent.</li></ol></li></ol></li></ol></li></ol></li><li>Else: <ol>
+ *   <li><a href="#event_systemAction"><code>systemAction</code> event</a> triggers
+ *   parameter payload <code>action</code> as <code>REJECT</code>.</li>
+ *   <li><b>ABORT</b> and return error.</li></ol></li></ol></li></ol>
  * @for Skylink
  * @since 0.5.5
  */
