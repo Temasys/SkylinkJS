40bed2f265bd0cb0136d61ae316b6455e57af7c1
Leticia Choo <leticia.choo@temasys.com.sg>
2016-09-26 15:01:08 +0800

    ESS-613 Added DATA_CHANNEL_STATE SEND_MESSAGE_ERROR.

--- source/data-channel.js
+++ source/data-channel.js
@@ -1,21 +1,27 @@
 /**
  * The list of Datachannel connection states.
  * @attribute DATA_CHANNEL_STATE
- * @param {String} CONNECTING <small>Value <code>"connecting"</code></small>
+ * @param {String} CONNECTING          <small>Value <code>"connecting"</code></small>
  *   The value of the state when Datachannel is attempting to establish a connection.
- * @param {String} OPEN       <small>Value <code>"open"</code></small>
+ * @param {String} OPEN                <small>Value <code>"open"</code></small>
  *   The value of the state when Datachannel has established a connection.
- * @param {String} CLOSING    <small>Value <code>"closing"</code></small>
+ * @param {String} CLOSING             <small>Value <code>"closing"</code></small>
  *   The value of the state when Datachannel connection is closing.
- * @param {String} CLOSED     <small>Value <code>"closed"</code></small>
+ * @param {String} CLOSED              <small>Value <code>"closed"</code></small>
  *   The value of the state when Datachannel connection has closed.
- * @param {String} ERROR      <small>Value <code>"error"</code></small>
+ * @param {String} ERROR               <small>Value <code>"error"</code></small>
  *   The value of the state when Datachannel connection has errors.
- * @param {String} CREATE_ERROR <small>Value <code>"createError"</code></small>
+ * @param {String} CREATE_ERROR        <small>Value <code>"createError"</code></small>
  *   The value of the state when Datachannel has failed to establish a connection.
  * @param {String} BUFFERED_AMOUNT_LOW <small>Value <code>"bufferedAmountLow"</code></small>
  *   The value of the state when Datachannel when the amount of data buffered to be sent
  *   falls below the Datachannel threshold.
+ *   <small>This state should occur only during after <a href="#method_sendBlobData">
+ *   <code>sendBlobData()</code> method</a> or <a href="#method_sendURLData"><code>sendURLData()</code> method</a> or
+ *   <a href="#method_sendP2PMessage"><code>sendP2PMessage()</code> method</a>.</small>
+ * @param {String} SEND_MESSAGE_ERROR  <small>Value <code>"sendMessageError"</code></small>
+ *   The value of the state when Datachannel when data transfer packets or P2P message fails to send
+ *   because Datachannel state is not at <code>OPEN</code>.
  *   <small>This state should occur only during after <a href="#method_sendBlobData">
  *   <code>sendBlobData()</code> method</a> or <a href="#method_sendURLData"><code>sendURLData()</code> method</a> or
  *   <a href="#method_sendP2PMessage"><code>sendP2PMessage()</code> method</a>.</small>
