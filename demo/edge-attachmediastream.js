864510cd85071d78eeb8bcc8e0b467d47fef540f
Leticia Choo <leticia.choo@temasys.com.sg>
2017-02-28 23:35:28 +0800

    Updates to demos to ensure it works accordingly in several scenarios.

--- demo/edge-attachmediastream.js
+++ demo/edge-attachmediastream.js
@@ -3,55 +3,56 @@
  * And Edge current does not support re-negotiation for now in the SDK.
  */
 (function () {
-  var attachMediaStream_base = attachMediaStream;
+  if (window.webrtcDetectedBrowser === 'edge') {
+    var attachMediaStream_base = attachMediaStream;
 
-  attachMediaStream = function (elm, stream) {
-    if (window.webrtcDetectedBrowser === 'edge' && elm && stream &&
-      typeof elm.tagName === 'string' && elm.tagName.toUpperCase() === 'VIDEO' &&
-      stream.getAudioTracks().length > 0 && stream.getVideoTracks().length > 0) {
-      try {
-        var audioStream = new MediaStream();
-        var videoStream = new MediaStream();
+    attachMediaStream = function (elm, stream) {
+      if (elm && stream && typeof elm.tagName === 'string' && elm.tagName.toUpperCase() === 'VIDEO' &&
+        stream.getAudioTracks().length > 0 && stream.getVideoTracks().length > 0) {
+        try {
+          var audioStream = new MediaStream();
+          var videoStream = new MediaStream();
 
-        stream.getTracks().forEach(function (t) {
-          if (t.kind === 'audio') {
-            audioStream.addTrack(t);
-          } else {
-            videoStream.addTrack(t);
-          }
-        });
+          stream.getTracks().forEach(function (t) {
+            if (t.kind === 'audio') {
+              audioStream.addTrack(t);
+            } else {
+              videoStream.addTrack(t);
+            }
+          });
 
-        attachMediaStream_base(elm, videoStream);
+          attachMediaStream_base(elm, videoStream);
 
-        var audioElm = document.createElement('audio');
-        audioElm.id = 'audio-' + Math.random() + '-' + Date.now();
-        audioElm.style.visibility = 'hidden';
-        audioElm.style.height = 0;
-        audioElm.style.width = 0;
-        audioElm.autoplay = true;
-        audioElm.muted = !!elm.getAttribute('muted');
+          var audioElm = document.createElement('audio');
+          audioElm.id = 'audio-' + Math.random() + '-' + Date.now();
+          audioElm.style.visibility = 'hidden';
+          audioElm.style.height = 0;
+          audioElm.style.width = 0;
+          audioElm.autoplay = true;
+          audioElm.muted = !!elm.getAttribute('muted');
 
-        document.body.appendChild(audioElm);
+          document.body.appendChild(audioElm);
 
-        attachMediaStream_base(audioElm, audioStream);
+          attachMediaStream_base(audioElm, audioStream);
 
-        var endedInterval = setInterval(function () {
-          if (typeof stream.elementRecordedTime === 'undefined') {
-            stream.elementRecordedTime = 0;
-          }
-          if (stream.elementRecordedTime === stream.currentTime) {
-            clearInterval(endedInterval);
-            document.body.removeChild(audioElm);
-          } else {
-            stream.elementRecordedTime = stream.currentTime;
-          }
-        }, 1000);
-      } catch (e) {
-        console.error('Failed appending workaround for Edge ->', e);
+          var endedInterval = setInterval(function () {
+            if (typeof stream.elementRecordedTime === 'undefined') {
+              stream.elementRecordedTime = 0;
+            }
+            if (stream.elementRecordedTime === stream.currentTime) {
+              clearInterval(endedInterval);
+              document.body.removeChild(audioElm);
+            } else {
+              stream.elementRecordedTime = stream.currentTime;
+            }
+          }, 1000);
+        } catch (e) {
+          console.error('Failed appending workaround for Edge ->', e);
+          attachMediaStream_base(elm, stream);
+        }
+      } else {
         attachMediaStream_base(elm, stream);
-      }
-    } else {
-      attachMediaStream_base(elm, stream);
-    } 
-  };
+      } 
+    };
+  }
 })();
