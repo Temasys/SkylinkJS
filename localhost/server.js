e36df1e9fd917d7e7c9715f2eac7e05e2d7ef0bd
Leticia Choo <leticia.choo@temasys.com.sg>
2017-03-10 21:52:29 +0800

    ESS-831 Updates of localhost/server.js for uncaught exception.

--- localhost/server.js
+++ localhost/server.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var fs = require('fs');
-var http = require('http');
-var https = require('https');
+var http = require('http'), https = require('https');
 var app = express();
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/../'));
 
 console.log('====== STARTING SERVERS =======');
-http.createServer(app).listen(8081);
-console.log('http server: 8081 port on localhost');
-
-https.createServer({
+var httpServer = http.createServer(app).listen(8081);
+var httpsServer = https.createServer({
   key: fs.readFileSync('localhost/server.key'),
   cert: fs.readFileSync('localhost/server.crt')
 }, app).listen(8082);
 
+process.on('uncaughtException', function () {
+  httpServer.close();
+  httpsServer.close();
+});
+
+console.log('http server: 8081 port on localhost');
 console.log('https server: 8082 port on localhost');
