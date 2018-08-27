e6ddcd045dcdbfe1b52067292dbb9d2197e886f6
avinash <>
2018-08-27 22:33:38 +0800

    added init option for enabling stats data

--- source/room-init.js
+++ source/room-init.js
@@ -41,6 +41,8 @@ Skylink.prototype.generateUUID = function() {
  *   <small>Note that this is a debugging feature and is only used when instructed for debugging purposes.</small>
  * @param {Boolean} [options.enableIceTrickle=true] The flag if Peer connections should
  *   trickle ICE for faster connectivity.
+ * @param {Boolean} [options.enableStats=true] The flag if SDK should gather stats information
+ *   and upload to Temasys server. By default, this option is true.
  * @param {Boolean} [options.enableDataChannel=true] <blockquote class="info">
  *   Note that for Edge browsers, this value is overriden as <code>false</code> due to its supports.
  *   </blockquote> The flag if Datachannel connections should be enabled.
