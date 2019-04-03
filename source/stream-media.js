6a4d59579e1b22ee151f26be4ffa9107de3f720d
ishannnaithani <ishan.naithani@temasys.io>
2019-04-03 17:13:22 +0800

    send-stream replace track

--- source/stream-media.js
+++ source/stream-media.js
@@ -495,29 +495,35 @@ Skylink.prototype.getUserMedia = function(options,callback) {
 Skylink.prototype.sendStream = function(options, callback) {
   var self = this;
 
-  var restartFn = function (stream) {
+  var performReplaceTracks = function (originalStream, newStream) {
+    var newStreamHasVideoTrack = Array.isArray(newStream.getVideoTracks()) && newStream.getVideoTracks().length;
+    var newStreamHasAudioTrack = Array.isArray(newStream.getAudioTracks()) && newStream.getAudioTracks().length;
+    var originalStreamHasVideoTrack = Array.isArray(originalStream.getVideoTracks()) && originalStream.getVideoTracks().length;
+    var originalStreamHasAudioTrack = Array.isArray(originalStream.getAudioTracks()) && originalStream.getAudioTracks().length;
+
+    if (newStreamHasVideoTrack && originalStreamHasVideoTrack) {
+      self._replaceTrack(originalStream.getVideoTracks()[0].id, newStream.getVideoTracks()[0]);
+    }
+
+    if (newStreamHasAudioTrack && originalStreamHasAudioTrack) {
+      self._replaceTrack(originalStream.getAudioTracks()[0].id, newStream.getAudioTracks()[0]);
+    }
+  };
+
+  var restartFn = function (originalStream, stream) {
     if (self._inRoom) {
+
       if (!self._streams.screenshare) {
         self._trigger('incomingStream', self._user.sid, stream, true, self.getPeerInfo(), false, stream.id || stream.label);
         self._trigger('peerUpdated', self._user.sid, self.getPeerInfo(), true);
+      } else {
+        performReplaceTracks(originalStream, stream);
       }
 
-      if (Object.keys(self._peerConnections).length > 0 || self._hasMCU) {
-        self._refreshPeerConnection(Object.keys(self._peerConnections), false, {}, function (err, success) {
-          if (err) {
-            log.error('Failed refreshing connections for sendStream() ->', err);
-            if (typeof callback === 'function') {
-              callback(new Error('Failed refreshing connections.'), null);
-            }
-            return;
-          }
-          if (typeof callback === 'function') {
-            callback(null, stream);
-          }
-        });
-      } else if (typeof callback === 'function') {
-        callback(null, stream);
+      if (self._streams.userMedia) {
+        performReplaceTracks(originalStream, stream);
       }
+
     } else if (typeof callback === 'function') {
       callback(null, stream);
     }
@@ -545,6 +551,16 @@ Skylink.prototype.sendStream = function(options, callback) {
       callback(new Error(edgeNotSupportError),null);
     }
     return;
+  }
+
+  var origStream = null;
+
+  if (self._streams.userMedia) {
+    origStream = self._streams.userMedia.stream;
+  }
+
+  if (self._streams.screenshare) {
+    origStream = self._streams.screenshare.stream;
   }
 
   if (typeof options.getAudioTracks === 'function' || typeof options.getVideoTracks === 'function') {
@@ -578,7 +594,7 @@ Skylink.prototype.sendStream = function(options, callback) {
       }
     }, false, false);
 
-    restartFn(options);
+    restartFn(origStream, options);
 
   } else {
     self.getUserMedia(options, function (err, stream) {
@@ -588,7 +604,7 @@ Skylink.prototype.sendStream = function(options, callback) {
         }
         return;
       }
-      restartFn(stream);
+      restartFn(origStream, stream);
     });
   }
 };
