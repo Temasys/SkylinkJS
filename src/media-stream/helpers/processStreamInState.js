94a10c3a912b914a849beee71903507e2b5c9dc9
dinawee <dinaw86@gmail.com>
2020-04-13 18:30:02 +0800

    fix(events): mediaAccessSuccess will have isAudio and isVideo flag in event payload

--- src/media-stream/helpers/processStreamInState.js
+++ src/media-stream/helpers/processStreamInState.js
@@ -6,6 +6,7 @@ import { mediaAccessSuccess } from '../../skylink-events';
 import { dispatchEvent } from '../../utils/skylinkEventManager';
 import PeerMedia from '../../peer-media/index';
 import { TAGS } from '../../constants';
+import { hasAudioTrack, hasVideoTrack } from '../../utils/helpers';
 
 const isStreamInState = (state, stream) => {
   const { streams } = state;
@@ -57,6 +58,8 @@ const processStreamInState = (stream = null, settings = {}, roomkey, isScreensha
     isScreensharing,
     isAudioFallback,
     streamId: stream.id,
+    isAudio: hasAudioTrack(stream),
+    isVideo: hasVideoTrack(stream),
   }));
 };
 
