d8446dc852d136cfb1ede61c8d5d7b9da6ed5ac3
dinawee <dinaw86@gmail.com>
2020-09-07 10:52:28 +0800

    fix(peerConnection): remove peerConnection leaveRoom and dispatch roomRejoin

--- src/peer-connection/helpers/peer-addition/callbacks/onconnectionstatechange.js
+++ src/peer-connection/helpers/peer-addition/callbacks/onconnectionstatechange.js
@@ -1,17 +1,13 @@
-import clone from 'clone';
 import { dispatchEvent } from '../../../../utils/skylinkEventManager';
-import { peerConnectionState, roomRejoin } from '../../../../skylink-events';
+import { peerConnectionState } from '../../../../skylink-events';
 import HandleIceConnectionStats from '../../../../skylink-stats/handleIceConnectionStats';
 import { PEER_CONNECTION_STATE, ICE_CONNECTION_STATE } from '../../../../constants';
 import logger from '../../../../logger';
 import MESSAGES from '../../../../messages';
-import SkylinkApiResponse from '../../../../models/api-response';
-import Room from '../../../../room';
 
 const onconnectionstatechange = (peerConnection, targetMid, state) => {
-  const { room, user } = state;
+  const { room } = state;
   const { connectionState, iceConnectionState } = peerConnection;
-  const { hasMCU } = new SkylinkApiResponse(null, room.id);
 
   // some states are not dispatched on oniceconnectionstatechange
   const handleIceConnectionStats = new HandleIceConnectionStats();
@@ -22,17 +18,6 @@ const onconnectionstatechange = (peerConnection, targetMid, state) => {
     state: connectionState,
     peerId: targetMid,
   }));
-
-  if (peerConnection.connectionState === PEER_CONNECTION_STATE.FAILED && peerConnection.iceConnectionState === ICE_CONNECTION_STATE.FAILED && !hasMCU) {
-    const roomInfo = clone(Room.getRoomInfo(room.id));
-    Room.leaveRoom(state)
-      .then(() => {
-        dispatchEvent(roomRejoin({
-          room: roomInfo,
-          peerId: user.sid,
-        }));
-      });
-  }
 };
 
 export default onconnectionstatechange;
