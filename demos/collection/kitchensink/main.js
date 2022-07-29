054256a5ea97f1eb01ad4077087f9598e0f5710d
dinawee <dinaw86@gmail.com>
2022-07-29 12:07:43 +0800

    Fixes for peerSession undefined when the peerSessionId is not persisted. Fallback to displaying the peerSessionId instead of display name

--- demos/collection/kitchensink/main.js
+++ demos/collection/kitchensink/main.js
@@ -656,7 +656,13 @@ SkylinkEventManager.addEventListener(SkylinkConstants.EVENTS.STORED_MESSAGES, (e
   })
 
   storedMessages.forEach((message) => {
-    const peerId = 'Stored Message' + ` [${(peerSessions[message.senderPeerId].isSelf ? 'You' : peerSessions[message.senderPeerId].displayName)}]`;
+    let peerId;
+    if (peerSessions[message.senderPeerId]) {
+      peerId = 'Stored Message' + ` [${(peerSessions[message.senderPeerId].isSelf ? 'You' : peerSessions[message.senderPeerId].displayName)}]`;
+    } else {
+      peerId = 'Stored Message' + ` [${message.senderPeerId}]`
+    }
+
     const content = `${message.isDataChannel ? 'P2P' : 'Socket'} -> GRP [${message['timeStamp'].replace('T', ", ").replace("Z", "")}] : ${message.content}`;
     Demo.Methods.displayChatMessage(peerId, content, message.isPrivate);
   })
